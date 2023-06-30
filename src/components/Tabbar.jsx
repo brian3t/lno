45d5e76fd52911451941238c166e90a5e9f21e8a
brian3t <ngxtri@gmail.com>
2023-06-29 21:45:35 -0700

    WIP: refresh trigger_reload_flag switch to useState

--- src/components/Tabbar.jsx
+++ src/components/Tabbar.jsx
@@ -14,12 +14,12 @@ export default (props) => {
       {/*<Link href="#" text="Events" iconIos="f7:envelope_fill"*/}
       {/*      iconAurora="f7:envelope_fill"*/}
       {/*      iconMd="material:email"></Link>*/}
-      <Link href="#" preventRouter onClick={() => {
+      {/*<Link href="#" preventRouter onClick={() => {
         // eslint-disable-next-line no-undef
         // f7router.navigate('/chat/')
         const sign_up_pr = document.getElementById('sign_up_pr')
         sign_up_pr.click()
-      }} text="Comment" iconF7="chat_bubble_2"></Link>
+      }} text="Comment" iconF7="chat_bubble_2"></Link>*/}
     </Toolbar>
   )
 }
--- src/pages/Eventt.jsx
+++ src/pages/Eventt.jsx
@@ -12,7 +12,7 @@ import {
   CardHeader,
   f7,
   Icon,
-  Link,
+  Link, List, ListInput,
   Navbar,
   NavLeft,
   NavRight,
@@ -25,12 +25,14 @@ import store from 'store2'
 import Tabbar from "../components/Tabbar"
 import {fm_date_time} from "@/jslib/helper"
 import apis from "@/jslib/rest_sc/apis"
+import arrowSwap from "framework7-icons/react/esm/ArrowSwap";
 // import './Event.less';
 
 apis.setup(ENV)
 
 const Eventt = (props) => {
   const {f7router} = props
+  let trigger_reload_flag = false
   let [event_comments, set_event_comments] = useState([])
   let {event_m, eventid} = props
   if (!eventid && !event_m) return (<Block>No data</Block>)
@@ -44,6 +46,7 @@ const Eventt = (props) => {
     f7router.navigate('/band/', {props: {band_m: first_band}})
     console.warn(`band clicked ${bandid}`)
   }
+  const [comment, set_comment] = useState('')
   const [faved, set_faved] = useState(false)
   const [popupOpened, setPopupOpened] = useState(false);
   const popup = useRef(null)
@@ -51,6 +54,7 @@ const Eventt = (props) => {
 
   useEffect(() => {
     console.log(`eventt use effect`)
+
     async function fetchData() {
       // You can await here
       const event_comments_res = await apis.g('event-comment', {event_id: event_m.id})
@@ -62,7 +66,7 @@ const Eventt = (props) => {
     //load_fav
     let is_faved = (store('fav_events') || []).includes(event_m.id)
     set_faved(is_faved)
-  }, [event_m.id, eventid])
+  }, [event_m.id, eventid, trigger_reload_flag])
 
   const createPopup = (event_comment) => {
     // Create popup
@@ -80,7 +84,7 @@ const Eventt = (props) => {
               </div>
               <div class="page-content">
                 <div class="block">
-                  <img src="https://socalappsolutions.com/p/${event_comment.created_by.toString().padStart(4, '0')}.jpg" alt="profile">
+                  <img src="https://socalappsolutions.com/p/${event_comment.created_by.toString().padStart(4, '0')}.jpg" alt="p">
                 </div>
               </div>
             </div>
@@ -90,6 +94,23 @@ const Eventt = (props) => {
     // }
     // Open it
     popup.current.open();
+  }
+
+  const comment_clicked = async function () {
+    const [comment_status, res] = await apis.c('event-comment', {
+      event_id: eventid,
+      comment: comment || 'I\'m going to this event'
+    })
+    if (!comment_status){
+      alert(res)
+      return false
+    }
+    f7.toast.create({
+      text: 'Comment added',
+      position: 'top',
+      closeTimeout: 2000,
+    }).open()
+    trigger_reload_flag = !trigger_reload_flag
   }
 
   /**
@@ -129,7 +150,7 @@ const Eventt = (props) => {
         <NavLeft backLink="Back" backLinkShowText={false}></NavLeft>
         <NavTitle>SD Events - Event</NavTitle>
         <NavRight>
-          <Link className="f7-icons" panelOpen="right"><i className="icon"></i></Link>
+          <Link className="f7-icons" iconF7="menu" icon="f7-panel" panelOpen="right"><i className="icon"></i></Link>
           {/*<a className="link icon-only" data-panel="right">
             <i className="icon"></i>
           </a>*/}
@@ -186,7 +207,7 @@ const Eventt = (props) => {
               <div className="message-avatar" onClick={() => createPopup(event_comment)}
                    style={{backgroundImage: `url("https://socalappsolutions.com/p/${event_comment.created_by.toString().padStart(4, '0')}_60x60.jpg")`}}></div>
               <div className="message-content">
-                <div className="message-name">Username redacted. Sign up to view username</div>
+                {/*<div className="message-name">Username redacted. Sign up to view username</div>*/}
                 <div className="message-bubble">
                   <div className="message-text"><span
                     slot="text">{event_comment.comment}</span>
@@ -195,6 +216,12 @@ const Eventt = (props) => {
               </div>
             </div>
           ) : ''}
+          <List>
+            <ListInput label="Comment" type="textarea" rows={2} placeholder="I'm going to this event"
+                       value={comment} onChange={(e => set_comment(e.target.value))}>
+            </ListInput>
+          </List>
+          <Button small outline onClick={comment_clicked}>Comment</Button>
         </Fragment>
         : <div> Loading..</div>
       }
